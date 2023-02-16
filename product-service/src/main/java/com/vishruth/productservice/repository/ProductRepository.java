896196cfe2584ae10bd013df4b4daaea673aba84
vishruthg18 <vishruthg18@gmail.com>
2023-02-15 20:54:53 -0500

    initial commit

---
+++ product-service/src/main/java/com/vishruth/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.vishruth.productservice.repository;
+
+import com.vishruth.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+}
