10f597ba3e6e033d867fec89567c805d19b1aa6f
vishruthg18 <vishruthg18@gmail.com>
2024-01-30 21:31:43 -0500

    Discovery service issues fix

--- product-service/src/main/java/com/vishruth/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/vishruth/productservice/controller/ProductController.java
@@ -16,16 +16,15 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {
-        productService.createProduct(productRequest);
+    public void createProduct(@RequestBody ProductRequest productRequest){
+            productService.createProduct(productRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts(){
-       return productService.getAllProducts();
+        return productService.getAllProducts();
     }
-
 }
